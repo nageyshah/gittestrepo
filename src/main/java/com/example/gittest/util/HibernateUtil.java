14a041b23269b6cde4b9b574eba31644479153a4
nageyshah <nagey.shaheen@gmail.com>
2016-12-28 13:49:26 -0500

    these 2 files will be committed from the laptop 1:49

--- src/main/java/com/example/gittest/util/HibernateUtil.java
+++ src/main/java/com/example/gittest/util/HibernateUtil.java
@@ -6,31 +6,34 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-    
-    private static SessionFactory buildSessionFactory() {
-        try {        	
-        	// Create the SessionFactory from hibernate.cfg.xml
-        	Configuration configuration = new Configuration().configure("hibernate.cfg.xml");     
-            return configuration.buildSessionFactory( new StandardServiceRegistryBuilder().applySettings( configuration.getProperties() ).build() );
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-	
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+			return configuration.buildSessionFactory(
+					new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build());
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void testFunctionAddedforGIT() {
+
+		Integer addedforGITLP;
+
+		for (int i = 0; i < 5; i++) {
+			// added as a function from laptop..
+		}
+
+	}
+
 }
-
-
-
-
-
-
-
